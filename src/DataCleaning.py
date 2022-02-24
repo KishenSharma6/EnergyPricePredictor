bb88fc9aef91118d9e609b8f65612034a96c1305
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-02-23 17:48:04 -0800

    Refactored code and added 3 seperate cleaning functions
    - clean_columns drops unneccessary or unwanted cols
    - set_time_index formats time col and sets as index for dataframe for
      easier visualization
    - kelvin_to_celsius

--- src/DataCleaning.py
+++ src/DataCleaning.py
@@ -1,12 +1,12 @@
 import string
 import pandas as pd
 import numpy as np
+import re
 
-def clean_energy_data(dataframe, cols = None):
-    """Takes pd.Dataframe containing raw energy data and performs the following opterations:
-        - Drops columns containing only null values
-        - Drops cols specified by cols
-        - Standardizes time and sets as index
+def clean_columns(dataframe, cols = None):
+    """Takes pd.Dataframe containing raw data and performs the following opterations:
+        - Drops cols specified by cols 
+        - Drops columns containing only null or non-unique values
     
     Functions returns cleaned energy dataframe
     Args:
@@ -14,14 +14,53 @@ def clean_energy_data(dataframe, cols = None):
         cols (list:strings): OPTIONAL. List of strings containing names of columns 
         you would like to be dropped
     """
-    #Check if arguments are correct datatypes
+    #Check data types of arguments. Drop cols if cols != None
     assert type(dataframe) == pd.DataFrame, "Error, dataframe is not pd.Dataframe"
+    
     if cols == None:
         pass
     else:
         assert all(type(c) == str for c in cols), "Error, cols can only contain strings"
+        dataframe= dataframe.drop(columns= [cols], axis= 1)
     
-    dataframe.dropna(how= "all", axis= 1, inplace= True)
-    dataframe.drop(columns= [cols], axis= 1, inplace= True)
+    #Drop cols containing null
+    dataframe= dataframe.dropna(how= "all", axis= 1)
 
+    #Drop cols containing single unique value
+    to_drop= []
+    for c in list(dataframe.columns):
+        if dataframe[c].nunique() == 1:
+            to_drop.append(c)
+    dataframe= dataframe.drop(columns = to_drop, axis= 1)
+
+    return dataframe
+
+
+def set_time_index(dataframe, time_column):
+    """Clean column containing time data you would like to set as an index
+
+    Args:
+        dataframe (pandas): pandas dataframe to apply transformation
+        time_column (string): name of column containing time data you would like cleaned and 
+        set as index
+
+    Returns:
+        _type_: _description_
+    """
+    #Clean time col and set as index
+    dataframe[time_column]= pd.to_datetime(dataframe[time_column].apply(lambda x: re.sub("[+].*", '', x )))
+    dataframe.set_index(time_column)
+    return dataframe
+
+def kelvin_to_celsius(dataframe, temp_cols):
+    """Convert cols containing kelvin temp to celsius
+
+    Args:
+        dataframe (pd.Dataframe): pandas dataframe containing data you would like cleaned
+        temp_col (string|list of strings): string (or list of strings) containing names of 
+        column(s) you would like transformation applied to
+    """
+    for c in temp_cols:
+        dataframe[c]= dataframe[c].apply(lambda x: x- 273.15)
     
+    return dataframe
