97e2e2355e7167b142f3486a8ec21f07756b7ba4
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-03-09 16:14:52 -0800

    First commit

---
+++ src/Visualizations.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+def heatmap(dataframe, figsize, annotate=True, linewidth= .2, cmap="coolwarm"):
+    f, ax= plt.subplots(figsize= figsize)
+    
+    corrMatrix = dataframe.corr()
+    sns.heatmap(corrMatrix, annot=annotate, linewidths= linewidth, cmap=cmap)
+    plt.show()   
+
+def corr_barh(dataframe, target, figsize):
+    f, ax= plt.subplots(figsize= figsize)
+    
+    corrMatrix = dataframe.corr()
+    corrMatrix[target][:-1].sort_values().plot(kind= "barh", ax=ax);
+
+
+def feature_distributions(dataframe, figsize= (20,40)):
+    cols= 4
+    rows= int(math.ceil(len(dataframe.columns)/cols))
+    f, axs= plt.subplots(rows,cols, figsize = figsize)
+    axs= axs.flatten()
+
+    for i, col in enumerate(dataframe.columns):
+        dataframe[col].plot.kde(ax= axs[i], color= "blue",)
+        axs[i].set_title(col)
+
+    plt.tight_layout()
